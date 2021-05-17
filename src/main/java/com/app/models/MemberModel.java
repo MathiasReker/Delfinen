a16e7f152c83d975ecb65f4937de0f08b042d789
MathiasReker <MathiasReker@users.noreply.github.com>
2021-05-17 22:07:37 +0000

    Google Java Format

--- src/main/java/com/app/models/MemberModel.java
+++ src/main/java/com/app/models/MemberModel.java
@@ -23,7 +23,7 @@ public class MemberModel {
   /**
    * Package Private constructor for instantiating objects in the model.
    *
-   * Can be used when reading stored data.
+   * <p>Can be used when reading stored data.
    *
    * @param ID
    * @param name
