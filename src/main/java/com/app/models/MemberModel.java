2b10e5a087837edcb44bec7567996a68cbfe03ed
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-05-17 09:42:10 +0200

    added Gender Enum and Member setters and Constructor

--- src/main/java/com/app/models/MemberModel.java
+++ src/main/java/com/app/models/MemberModel.java
@@ -1,24 +1,80 @@
 package com.app.models;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class MemberModel {
-    private String ID;
-    private String name;
-    private LocalDateTime age;
-    private Gender gender;
-    private string phoneNumber;
-    private CompetetivenessLevel level;
-    private ArrayList<Disciplines> disciplines;
-    private final LocalDateTime creationDate;
-    private ArrayList<MembershipModel> memberships;
+  private String ID;
+  private String name;
+  private LocalDate age;
+  private GenderModel gender;
+  private String phoneNumber;
+  private boolean competitive;
+  private ArrayList<DisciplineModel> disciplines = new ArrayList<>();
+  private final LocalDate creationDate;
+  private ArrayList<MembershipModel> memberships = new ArrayList<>();
 
+  public MemberModel() {
+    creationDate = LocalDateTime.now().toLocalDate();
+  }
 
-    public MemberModel(){
-        creationDate = LocalDateTime.now();
-    }
+  MemberModel(String ID,
+              String name,
+              LocalDate age,
+              GenderModel gender,
+              String phoneNumber,
+              boolean competitive,
+              ArrayList<DisciplineModel> disciplines,
+              LocalDate creationDate,
+              ArrayList<MembershipModel> memberships){
+    setID(ID);
+    setName(name);
+    setAge(age);
+    setGender(gender);
+    setPhoneNumber(phoneNumber);
+    setCompetitive(competitive);
+    this.creationDate = creationDate;
+  }
 
+  public void setID(String ID) {
+    this.ID = ID;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setAge(LocalDate age) {
+    this.age = age;
+  }
+
+  public void setGender(GenderModel gender) {
+    this.gender = gender;
+  }
+
+  public void setCompetitive(boolean competitive) {
+    this.competitive = competitive;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public void addMembership(MembershipModel membership){
+    memberships.add(membership);
+  }
+
+  public void addDiscipline(DisciplineModel discipline){
+    disciplines.add(discipline);
+  }
+
+  void setDisciplines(ArrayList<DisciplineModel> disciplines){
+    this.disciplines = disciplines;
+  }
+
+  void setMemberships(ArrayList<MembershipModel> memberships){
+    this.memberships = memberships;
+  }
 
 }
-
