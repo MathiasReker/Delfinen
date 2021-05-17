5756f951b74e2dd592a158ab08edf75254a960c8
Mohamad Ibrahim <mohamedibr@gmail.com>
2021-05-17 12:42:56 +0200

    first push of compition and result class

--- src/main/java/com/app/models/CompetitionModel.java
+++ src/main/java/com/app/models/CompetitionModel.java
@@ -1,3 +1,74 @@
 package com.app.models;
 
-public class CompetitionModel {}
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+
+
+public class CompetitionModel {
+
+  private String id;
+  private LocalDate startDate;
+  private String name;
+  private LocalTime startTime;
+  private ArrayList<ResultModel> result = new ArrayList<>();
+
+
+  public CompetitionModel(LocalDate startDate, String name, LocalTime startTime) {
+    setId("test");
+    this.startDate = startDate;
+    this.name = name;
+    this.startTime = startTime;
+  }
+
+  //getter
+
+
+  public String getId() {
+    return id;
+  }
+
+  public LocalDate getStartDate() {
+    return startDate;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public LocalTime getStartTime() {
+    return startTime;
+  }
+
+  public ArrayList<ResultModel> getResult() {
+    return result;
+  }
+
+  //setter
+  public void setId(String id) { //TODO how will we manage the ids for competitions? will need to be refactored
+    this.id = id;
+  }
+
+  public void setStartDate(LocalDate startDate) {
+    this.startDate = startDate;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setStartTime(LocalTime startTime) {
+    this.startTime = startTime;
+  }
+
+  public void setResult(ArrayList<ResultModel> result) {
+    this.result = result;
+  }
+
+  public void addResult(ResultModel result) {
+    this.result.add(result);
+  }
+
+}
---
+++ src/main/java/com/app/views/CompetitionView.java
@@ -0,0 +1,4 @@
+package com.app.views;
+
+public class CompetitionView {
+}
