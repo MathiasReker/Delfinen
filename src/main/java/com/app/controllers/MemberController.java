56c3eed464746c2ff0067968e6d096c1b708151f
Mathias Reker <mathias@reker.dk>
2021-05-18 10:43:19 +0200

    Add validation

--- src/main/java/com/app/controllers/MemberController.java
+++ src/main/java/com/app/controllers/MemberController.java
@@ -4,6 +4,8 @@ import com.app.models.GenderModel;
 import com.app.models.MemberModel;
 import com.app.views.MemberView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Scanner;
 
@@ -16,7 +18,40 @@ public class MemberController {
     MEMBER_VIEW = new MemberView();
   }
 
-  private String[] genders() {
+  public void createMember(Scanner in) {
+
+    MEMBER_VIEW.printInline("ID: ");
+    String id = in.nextLine();
+
+    MEMBER_VIEW.printInline("Name: ");
+    String name = validateName(in);
+
+    MEMBER_VIEW.displayGenderMenu(gendersToArray());
+    int genderIndex = in.nextInt();
+    in.nextLine();
+
+    MEMBER_VIEW.printInline("Birthday [dd/MM/yyyy]: ");
+    String birthday = validateDate(in);
+
+    MEMBER_VIEW.printInline("Phone: ");
+    String phone = validatePhoneNumber(in);
+
+    MEMBER_VIEW.printInline("Mail: ");
+    String mail = validateMail(in);
+
+    MEMBER_VIEW.printInline("Competitive [Y/n]: ");
+    boolean competitive = promptYesNo(in);
+
+    MEMBER.setID(id);
+    MEMBER.setName(name);
+    MEMBER.setGender(GenderModel.values()[genderIndex]);
+      MEMBER.setBirthdate(LocalDate.parse(birthday));
+    MEMBER.setPhoneNumber(phone);
+    MEMBER.setMail(mail);
+    MEMBER.setCompetitive(competitive);
+  }
+
+  private String[] gendersToArray() {
     String[] result = new String[GenderModel.values().length];
 
     for (int i = 0; i < result.length; i++) {
@@ -26,14 +61,61 @@ public class MemberController {
     return result;
   }
 
-  private boolean yesNo(Scanner in) {
+  private String validateName(Scanner in) {
+    while (!in.hasNext("[a-zA-Z]+")) {
+      MEMBER_VIEW.printInlineWarning("Not a valid name. Please try again: ");
+      in.nextLine();
+    }
+    return in.nextLine();
+  }
+
+  private String validateMail(Scanner in) {
+    while (!in.hasNext("[A-Za-z0-9+_.-]+@(.+)")) {
+      MEMBER_VIEW.printInlineWarning("Not a valid mail. Please try again: ");
+      in.nextLine();
+    }
+    return in.nextLine();
+  }
+
+  private String validatePhoneNumber(Scanner in) {
+    while (!in.hasNext("([+](\\d{1,3})\\s?)?((\\(\\d{3,5}\\)|\\d{3,5})(\\s)?)\\d{3,8}")) {
+      MEMBER_VIEW.printInlineWarning("Not a valid phone number. Please try again: ");
+      in.nextLine();
+    }
+    return in.nextLine();
+  }
+
+  private String validateDate(Scanner in) {
+    while (!isValidDate(in.next())) {
+      MEMBER_VIEW.printInlineWarning("Not a valid date. Please try again: ");
+      in.nextLine();
+    }
+    return in.nextLine();
+  }
+
+  private boolean isValidDate(String date) {
+    if (!date.trim().equals("")) {
+      SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+      simpleDateFormat.setLenient(false);
+      try {
+        simpleDateFormat.parse(date);
+        return true;
+      } catch (ParseException e) {
+        return false;
+      }
+    }
+
+    return false;
+  }
+
+  private boolean promptYesNo(Scanner in) {
     String input = in.nextLine();
     while (!input.equalsIgnoreCase("y") && !input.equalsIgnoreCase("n")) {
-      MEMBER_VIEW.printInlineWarning("Not a valid menu choice. Please try again: ");
+      MEMBER_VIEW.printInlineWarning("Not a valid choice. Please try again: ");
 
-      if (input.equals("Y")) {
+      if (input.equalsIgnoreCase("y")) {
         return true;
-      } else if (input.equals("N")) {
+      } else if (input.equalsIgnoreCase("N")) {
         return false;
       } else {
         input = in.nextLine();
@@ -41,41 +123,5 @@ public class MemberController {
     }
 
     return false;
-  }
-
-  public void createMember(Scanner in) {
-
-    System.out.println(yesNo(in));
-    // Asks for input constantly until it gets y or n
-
-    MEMBER_VIEW.printInline("ID: ");
-    String id = in.nextLine();
-
-    MEMBER_VIEW.printInline("Name: ");
-    String name = in.nextLine();
-
-    MEMBER_VIEW.displayGenderMenu(genders());
-    int gender = in.nextInt();
-    in.nextLine();
-
-    MEMBER_VIEW.printInline("Birthday [like 9/10/1992]: ");
-    String birthday = in.nextLine();
-
-    MEMBER_VIEW.printInline("Phone: ");
-    String phone = in.nextLine();
-
-    MEMBER_VIEW.printInline("Mail: ");
-    String mail = in.nextLine();
-
-    MEMBER_VIEW.printInline("Competitive [Y/n]: ");
-    boolean competitive = false;
-
-    MEMBER.setID(id);
-    MEMBER.setName(name);
-    MEMBER.setGender(GenderModel.values()[gender]);
-    MEMBER.setBirthdate(LocalDate.parse(birthday));
-    MEMBER.setPhoneNumber(phone);
-    MEMBER.setMail(mail);
-    MEMBER.setCompetitive(competitive);
   }
 }
