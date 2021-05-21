1b991338d51b3f597f5f10fd2a3d9cd77a9a4f54
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-05-21 18:52:45 +0200

    beatyfication

--- src/main/java/com/app/views/PredictionView.java
+++ src/main/java/com/app/views/PredictionView.java
@@ -1,30 +1,16 @@
 package com.app.views;
 
+import java.math.BigDecimal;
+
 public class PredictionView extends View {
 
-  /**
-   * Returns a string in a danish currency format
-   *
-   * @param prediction predicted price to format
-   * @return
-   */
-  private String formatPrediction(int prediction) {
-    String result;
-    result = String.valueOf(prediction);
-    if (result.length() < 2) {
-      result = "0,0" + result;
-    } else if (result.length() < 3) {
-      result = "0," + result;
-    } else {
-      result =
-          result.substring(0, result.length() - 2) + "," + result.substring(result.length() - 2);
-    }
-    return result;
+  private float movePointLeft(int prediction) {
+    return new BigDecimal(prediction).movePointLeft(2).floatValue();
   }
 
   public void printExpectedIncome(int prediction, int days) {
     String dayOrDays = days == 1 ? "day" : "days";
     print("Expected income in " + days + " " + dayOrDays);
-    print(formatPrediction(prediction) + "kr");
+    System.out.printf("%.2f kr",movePointLeft(prediction));
   }
 }
