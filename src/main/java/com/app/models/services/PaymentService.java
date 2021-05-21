b38ae5acc2bedaceddaa3a6082ce758cba462ea9
Moshizzl3 <Moshizzl3@users.noreply.github.com>
2021-05-21 14:52:58 +0000

    Google Java Format

--- src/main/java/com/app/models/services/PaymentService.java
+++ src/main/java/com/app/models/services/PaymentService.java
@@ -25,8 +25,7 @@ public class PaymentService {
     return result;
   }
 
-    public void backupToFile(ArrayList<String> strings)
-        throws IOException {
+  public void backupToFile(ArrayList<String> strings) throws IOException {
     FileService fileService = new FileService(createNewPath());
     String[] result = new String[strings.size()];
 
@@ -36,11 +35,11 @@ public class PaymentService {
     fileService.writeToFile(result);
   }
 
-  private String createNewPath(){
+  private String createNewPath() {
     String s = path;
-    String [] data = s.split("/");
+    String[] data = s.split("/");
     data[2] = "backup.txt";
-    s = String.join("/", data[0],data[1],LocalDate.now() + data[2]);
+    s = String.join("/", data[0], data[1], LocalDate.now() + data[2]);
     return s;
   }
 }
