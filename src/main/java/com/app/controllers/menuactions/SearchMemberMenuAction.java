5c0b260187be6901a978fa20f5f6705c2c7d5137
Mathias Reker <mathias@reker.dk>
2021-05-23 20:54:52 +0200

    fix bug in action

--- src/main/java/com/app/controllers/menuactions/SearchMemberMenuAction.java
+++ src/main/java/com/app/controllers/menuactions/SearchMemberMenuAction.java
@@ -3,16 +3,11 @@ package com.app.controllers.menuactions;
 import com.app.controllers.MemberController;
 
 public class SearchMemberMenuAction extends MenuActions {
-  /**
-   * Stop the program from running.
-   *
-   * @param itemName String that describes the menu action.
-   */
+
   public SearchMemberMenuAction(String itemName) {
-    super(itemName, false);
+    super(itemName);
   }
 
-  /** No business logic should be done as this menu action shut down the program. */
   @Override
   public void run() {
     new MemberController().viewMemberByName();
