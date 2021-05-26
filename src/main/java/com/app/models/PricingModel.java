edfd18c5611cd724f5e34864ecfc8a31dc507b0a
Mathias Reker <mathias@reker.dk>
2021-05-26 12:28:49 +0200

    discount

--- src/main/java/com/app/models/PricingModel.java
+++ src/main/java/com/app/models/PricingModel.java
@@ -3,23 +3,25 @@ package com.app.models;
 import com.app.models.types.AgeGroupType;
 
 public class PricingModel {
-  private static final int DISCOUNT_PERCENT = 25; // discount in percent
-
   /**
    * Returns hardcoded price based on requirements
    *
    * @param member member to get price of.
    * @return price
    */
-  public static int calculateMemberPrice(MemberModel member) {
+  public static int getMemberPrice(MemberModel member) {
     if (member.getAgeGroup().equals(AgeGroupType.JUNIOR)) {
       return AgeGroupType.JUNIOR.getPrice();
     } else {
       if (member.getAge() > 60) {
-        return (int) (AgeGroupType.SENIOR.getPrice() * (100 - DISCOUNT_PERCENT) / 100.0);
+        return getPriceWithDiscount(AgeGroupType.SENIOR.getPrice(), 25);
       }
 
       return AgeGroupType.SENIOR.getPrice();
     }
+  }
+
+  private static int getPriceWithDiscount(int price, int discountPercent) {
+    return (int) (price * (100 - discountPercent) / 100.0);
   }
 }
